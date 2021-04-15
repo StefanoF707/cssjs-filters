f07868818bb3393c2dbf1f9ac167b7803f0ac027
StefanoF707 <stefano.franchini77@gmail.com>
2021-04-16 01:18:46 +0200

    range inputs

--- js/main.js
+++ js/main.js
@@ -5,35 +5,43 @@ window.addEventListener('load', () =>  {
    const filterList = [
       {
          name: 'Grayscale',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Saturate',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Sepia',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Invert',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Contrast',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Brightness',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Blur',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
       {
          name: 'Hue-rotate',
-         defaultValue: 0
+         defaultValue: 0,
+         open: false,
       },
    ];
 
@@ -48,19 +56,26 @@ window.addEventListener('load', () =>  {
       imgLoaded = true;
 
       if (imgLoaded) {
-         filterList.forEach( (element) => {
-            document.getElementById('filters-list').innerHTML += 
-            `
-               <li>
-                  <h3 class="filter-name">
-                     ${element.name}
-                  </h3>
-                  <input type="number" value="${element.defaultValue}">
-               </li>
-            `
-         });
+         createListElements(filterList);
       }
    });
 
    
 });
+
+
+// FUNCTIONS----------------------------------
+
+function createListElements(array) {
+   array.forEach( (element) => {
+      document.getElementById('filters-list').innerHTML += 
+      `
+         <li>
+            <h3 class="filter-name">
+               ${element.name}
+            </h3>
+            <input type="range" value="${element.defaultValue}">
+         </li>
+      `
+   });
+}
